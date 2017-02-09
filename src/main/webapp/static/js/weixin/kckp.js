423e1971f035f2520341b018b48dacc7183ec2ab
chensiyao <1076665044@qq.com>
2017-02-09 23:05:54 +0800

    Signed-off-by: chensiyao <1076665044@qq.com>
    禁止将appsercet、access_token存入客户端

--- src/main/webapp/static/js/weixin/kckp.js
+++ src/main/webapp/static/js/weixin/kckp.js
@@ -5,22 +5,5 @@
  */
 $(function(){
 	
-	$.ajax({
-		type: "post",
-		url: "https://api.weixin.qq.com/sns/oauth2/access_token",
-		data: {
-			appid: $("#wx_appid").val(),
-			secret: $("#wx_appsecret").val(),
-			code: $("#wx_code").val(),
-			grant_type: "authorization_code"
-		},
-		async: false,
-		success: function(rst){
-			alert(rst);
-		},
-		error: function(e1,e2,e3){
-			alert(e1);
-			alert(e2);
-		}
-	});
+	
 });
