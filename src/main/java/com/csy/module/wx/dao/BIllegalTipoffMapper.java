9038c8eb5a36767e5b730efebae6ebbfdf3b2724
guwenwen <guwenwen@kedacom.com>
2017-08-23 10:12:28 +0800

    违法举报管理代码

--- src/main/java/com/csy/module/wx/dao/BIllegalTipoffMapper.java
+++ src/main/java/com/csy/module/wx/dao/BIllegalTipoffMapper.java
@@ -1,9 +1,19 @@
 package com.csy.module.wx.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.csy.module.wx.entity.BIllegalTipoff;
 import com.csy.module.wx.entity.BIllegalTipoffExample;
 import com.csy.util.spring.BaseDao;
 
 public interface BIllegalTipoffMapper extends BaseDao<BIllegalTipoff, BIllegalTipoffExample>{
+
+
+
+	List<BIllegalTipoff> selectTipoffBysj(@Param("kssj")String kssj,@Param("jssj") String jssj,@Param("status")String status);
+
+
   
 }
