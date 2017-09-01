a8d50aa984d40e80808684a94700505219f09089
pikaka <10766665044@qq.com>
2017-09-01 16:25:50 +0800

    故障上报

---
+++ src/main/java/com/csy/module/wx/dao/BFailureReportMapper.java
@@ -0,0 +1,9 @@
+package com.csy.module.wx.dao;
+
+import com.csy.module.wx.entity.BFailureReport;
+import com.csy.module.wx.entity.BFailureReportExample;
+import com.csy.util.spring.BaseDao;
+
+public interface BFailureReportMapper extends BaseDao<BFailureReport, BFailureReportExample>{
+  
+}
